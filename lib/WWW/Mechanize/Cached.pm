721db7162ade11a666d7eeed3ff2a24287eae066
Andy Lester <andy@petdance.com>
2004-01-19 05:14:05 +0000

    Import of PETDANCE/WWW-Mechanize-Cached-1.24 from CPAN.
    gitpan-cpan-distribution: WWW-Mechanize-Cached
    gitpan-cpan-version:      1.24
    gitpan-cpan-path:         PETDANCE/WWW-Mechanize-Cached-1.24.tar.gz
    gitpan-cpan-author:       PETDANCE
    gitpan-cpan-maturity:     released

--- lib/WWW/Mechanize/Cached.pm
+++ lib/WWW/Mechanize/Cached.pm
@@ -1,69 +1,22 @@
 package WWW::Mechanize::Cached;
+
 use strict;
 use warnings FATAL => 'all';
-use vars qw( $VERSION );
-$VERSION = '1.23';
-
-use base qw( WWW::Mechanize );
-use Carp qw( carp croak );
-use Storable qw( freeze thaw );
-use Cache::Cache;
-
-my %default = (
-    class => 'Cache::FileCache',
-    args => {
-        namespace => __PACKAGE__,
-        default_expires_in => "1d",
-    }
-);
-my $key = __PACKAGE__;
-
-sub new
-{
-    my $class = shift;
-    my %args = @_;
-    my %opts = %default;
-    if (exists $args{cache})
-    {
-        my %new = %{ delete $args{cache} };
-        $opts{class} = delete $new{class} if exists $new{class};
-        %{$opts{args}} = ( %{ $opts{args} }, %new );
-        croak "Bad class name" unless $opts{class} =~ /^[\w:]+$/;
-    }
-    my $self = $class->SUPER::new( %args );
-    eval "use $opts{class}";
-    my $c = $opts{class}->new( $opts{args} );
-    $self->{$key} = $c;
-    return $self;
-}
-
-sub _make_request
-{
-    my $self = shift;
-    my $c = $self->{$key};
-    my $request = shift;
-    my $key = $request->as_string;
-    my $v = $c->get( $key );
-    if ($v) {
-        $v = thaw $v;
-    } else {
-        $v = $self->SUPER::_make_request( $request, @_ );
-        $c->set( $key, freeze($v) );
-    };
-
-    # An odd line to need.
-    $self->{proxy} = {} unless defined $self->{proxy};
-
-    return $v;
-}
-
-1;
-
-__END__
 
 =head1 NAME
 
-WWW::Mechanize::Cached - Cache resposne to be polite
+WWW::Mechanize::Cached - Cache response to be polite
+
+=head1 VERSION
+
+Version 1.24
+
+    $Header: /home/cvs/www-mechanize-cached/lib/WWW/Mechanize/Cached.pm,v 1.5 2004/01/19 05:00:41 andy Exp $
+
+=cut
+
+use vars qw( $VERSION );
+$VERSION = '1.24';
 
 =head1 SYNOPSIS
 
@@ -85,6 +38,22 @@ WWW::Mechanize::Cached - Cache resposne to be polite
 Uses the L<Cache::Cache> hierarchy to implement a caching
 Mech. This lets one perform repeated requests without
 hammering a server impolitely.
+
+=cut
+
+use base qw( WWW::Mechanize );
+use Carp qw( carp croak );
+use Storable qw( freeze thaw );
+use Cache::Cache;
+
+my %default = (
+    class => 'Cache::FileCache',
+    args => {
+        namespace => __PACKAGE__,
+        default_expires_in => "1d",
+    }
+);
+my $key = __PACKAGE__;
 
 =head1 CONSTRUCTOR
 
@@ -110,10 +79,52 @@ C<default_expires_in> (set to C<1d>).
 
 =back
 
+=cut
+
+sub new {
+    my $class = shift;
+    my %args = @_;
+    my %opts = %default;
+    if (exists $args{cache}) {
+        my %new = %{ delete $args{cache} };
+        $opts{class} = delete $new{class} if exists $new{class};
+        %{$opts{args}} = ( %{ $opts{args} }, %new );
+        croak "Bad class name" unless $opts{class} =~ /^[\w:]+$/;
+    }
+    my $self = $class->SUPER::new( %args );
+    eval "use $opts{class}";
+    my $c = $opts{class}->new( $opts{args} );
+    $self->{$key} = $c;
+    return $self;
+}
+
 =head1 METHODS
 
 All methods are provided by L<WWW::Mechanize>. See taht module's
 documentation for details.
+
+=cut
+
+sub _make_request {
+    my $self = shift;
+    my $c = $self->{$key};
+    my $request = shift;
+    my $key = $request->as_string;
+    my $v = $c->get( $key );
+    if ($v) {
+        $v = thaw $v;
+    } else {
+        $v = $self->SUPER::_make_request( $request, @_ );
+        $c->set( $key, freeze($v) );
+    };
+
+    # An odd line to need.
+    $self->{proxy} = {} unless defined $self->{proxy};
+
+    return $v;
+}
+
+
 
 =head1 THANKS
 
@@ -141,7 +152,7 @@ your problem is less likely to be neglected.
 
 =head1 LICENCE AND COPYRIGHT
 
-This module is copyright E<copy> Iain Truskett, 2003. All rights
+This module is copyright Iain Truskett and Andy Lester, 2004. All rights
 reserved.
 
 This library is free software; you can redistribute it and/or modify
@@ -154,10 +165,13 @@ L<perlgpl> as supplied with Perl 5.8.1 and later.
 
 =head1 AUTHOR
 
-Iain Truskett <spoon@cpan.org>
+Iain Truskett <spoon@cpan.org>, currently maintained by Andy Lester
+<petdance@cpan.org>
 
 =head1 SEE ALSO
 
 L<perl>, L<WWW::Mechanize>.
 
 =cut
+
+"We'll miss you, Spoon";
